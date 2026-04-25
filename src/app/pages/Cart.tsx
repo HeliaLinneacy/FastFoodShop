ff4e614693ce75b0588c91d2d4d63d2af75a4dca
nguyenducthang1204205-boop <nguyenducthang1204205@gmail.com>
2026-04-26 00:03:26 +0700

    Update Cart.tsx

--- src/app/pages/Cart.tsx
+++ src/app/pages/Cart.tsx
@@ -133,8 +133,8 @@ export function Cart() {
               
               <div className="space-y-3 mb-6">
                 <div className="flex justify-between">
-                  <span className="text-gray-600">Tạm tính:</span>
-                  <span className="font-medium">{formatPrice(total)}</span>
+                  <span>Tạm tính:</span>
+                  <span>{formatPrice(total)}</span>
                 </div>
                 <div className="flex justify-between">
                   <span className="text-gray-600">Phí vận chuyển:</span>
