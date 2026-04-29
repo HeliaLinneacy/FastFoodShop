1ff65ae0a5e238636a7fd971aa49d4c089688e43
nguyenducthang1204205-boop <nguyenducthang1204205@gmail.com>
2026-04-29 23:52:36 +0700

    Update AdminOrders.tsx

--- src/app/pages/admin/AdminOrders.tsx
+++ src/app/pages/admin/AdminOrders.tsx
@@ -51,7 +51,7 @@ export function AdminOrders() {
     if (selectedOrder?.id === orderId) {
       setSelectedOrder({ ...selectedOrder, status: newStatus });
     }
-  };
+  }, [updateOrder, selectedOrder]);
 
   return (
     <div>
